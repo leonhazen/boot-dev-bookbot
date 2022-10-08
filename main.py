d15a34006053f81b1483c7de851caed12e0d24e1
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2022-10-08 16:28:15 +1100

    Output stats about each letter

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ def countLetters(text):
     return letterCount
 
 with open('./books/frankenstein.txt') as f:
+    # Read in book file
     file_contents = f.read()
     
+    # Split into list of words
     words = file_contents.split()
     
+    # Print the count of words
     print('This book has ' + str(len(words)) + ' words.')
 
+    # Run the countLetters function to create dictionary of letters and their counts
     letterDict = countLetters(file_contents)
-    print(letterDict)
+
+    # Sort the letters into an ordered list based on the count of that letter
+    sortedLetters = sorted(letterDict, key=letterDict.get, reverse=True)
+
+    # Loop through the ordered list of letters and print the count for each of them
+    for letter in sortedLetters:
+        print("The '" + letter + "' character was found " + str(letterDict[letter]) + " times")
