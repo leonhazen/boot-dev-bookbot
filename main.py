8b241676c3d6d125eb51eef19ef57aa5e470837c
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2022-10-08 16:16:33 +1100

    Count letter function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import string
+
+def countLetters(text):
+    letterCount = {}
+    for letter in list(string.ascii_lowercase):
+        letterCount[letter] = text.lower().count(letter)
+
+    return letterCount
 
 with open('./books/frankenstein.txt') as f:
     file_contents = f.read()
@@ -5,3 +13,6 @@ with open('./books/frankenstein.txt') as f:
     words = file_contents.split()
     
     print('This book has ' + str(len(words)) + ' words.')
+
+    letterDict = countLetters(file_contents)
+    print(letterDict)
